5dd3cfbd270bac7968e3a5b289e70e73ec23ff25
maubmz <mauriciobaeza488@gmail.com>
2025-06-12 13:48:53 -0600

    cambios en spring template

--- template-spring/content/demo/src/main/java/com/example_test/${{values.name}}Application.java
+++ template-spring/content/demo/src/main/java/com/example_test/${{values.name}}Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class ${{values.name}}Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(${{values.name}}Application.class, args);
 	}
 
 }

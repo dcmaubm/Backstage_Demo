1dbfd9099835fac3e89161c231f3d55296bd6b90
maubmz <mauriciobaeza488@gmail.com>
2025-06-12 14:34:59 -0600

    agregando campo a form

--- template-spring/content/{{ values.nameProject }}/src/main/java/com/example_test/${{values.nameProject}}Application.java
+++ template-spring/content/{{ values.nameProject }}/src/main/java/com/example_test/${{values.nameProject}}Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ${{values.name}}Application {
+public class ${{values.nameProject}}Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(${{values.name}}Application.class, args);
+		SpringApplication.run(${{values.nameProject}}Application.class, args);
 	}
 
 }
